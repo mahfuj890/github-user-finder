3b7a1229f76d4f7419a2838ab6675fc17c04fbbf
mahfuj890 <mahfujali490@gmail.com>
2022-02-01 15:58:31 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <button className="btn">Click</button>
+    </div>
+  );
+}
+
+export default App;
